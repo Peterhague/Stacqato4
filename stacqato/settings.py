fecd8dc3ee01446dd9f461abbc2a6419af4a02b8
Peterhague <prhague@hotmail.co.uk>
2022-05-10 14:57:09 +0000

    added python packages

--- stacqato/settings.py
+++ stacqato/settings.py
@@ -130,3 +130,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://8000-peterhague-stacqato4-4lfuuagnowo.ws-eu44.gitpod.io']
